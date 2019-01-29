2984cd54b6abf07b3e9d20e2f57e1635dca5a53b
andreahowes <ashcreek.howes@gmail.com>
2019-01-29 10:03:18 -0500

    began weather api creation

---
+++ src/main/java/com/andreah/oauth2tutorial20190109/data/ApiWeatherRepository.java
@@ -0,0 +1,45 @@
+package com.andreah.oauth2tutorial20190109.data;
+
+
+import com.andreah.oauth2tutorial20190109.service.Weather;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+
+import java.net.URI;
+
+@Repository
+@PropertySource("classpath:weatherAPIapplication.properties")
+public class ApiWeatherRepository {
+    private static final String PATH = "weather";
+    private static final String QUERY = "q";
+    @Value("${api.weather.baseurl}")
+    private String BASE_URL;
+    @Value("${api.weather.apikey.param}")
+    private String APPID;
+    @Value("${api.weather.apikey.value}")
+    private String API_KEY;
+    private RestTemplate restTemplate;
+
+  // @Autowired
+  // public ApiWeatherRepository(RestTemplate restTemplate) {
+  //     this.restTemplate = restTemplate;
+  // }
+
+
+  // @Override
+  // public Weather getWeather(String location) {
+  //     URI uri = new DefaultUriBuilderFactory()
+  //             .uriString(BASE_URL)
+  //             .path(PATH)
+  //             .queryParam(QUERY, location)
+  //             .queryParam(APPID, API_KEY)
+  //             .build();
+  //     ApiWeather apiWeather = restTemplate.getForObject(uri, ApiWeather.class);
+  //     Weather weather = WeatherMapper.mapWeather(apiWeather);
+  //     return weather;
+  // }
+}
